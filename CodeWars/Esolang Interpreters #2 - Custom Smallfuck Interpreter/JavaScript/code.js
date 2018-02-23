1c1c0ef078969cb02da914f28bbee4e543c7aec5
Joshua Saucedo <joshuansu0897@gmail.com>
2018-02-23 08:46:09 -0700

    cambios al problema, pasan los test, me falta hacerlo mas eficiente

--- CodeWars/Esolang Interpreters #2 - Custom Smallfuck Interpreter/JavaScript/code.js
+++ CodeWars/Esolang Interpreters #2 - Custom Smallfuck Interpreter/JavaScript/code.js
@@ -2,6 +2,40 @@ module.exports = function interpreter(code, tape) {
     let cod = code.split('')
     let tap = tape.split('')
 
-    console.log(cod);
-    console.log(tap);
+    let posTab = 0
+
+    for (let i = 0; i < cod.length; i++) {
+
+        if (posTab < 0 || posTab == tap.length) {
+            return tap.join("")
+        }
+
+        switch (cod[i]) {
+            case '>':
+                posTab++
+                break;
+            case '<':
+                posTab--
+                break;
+            case '*':
+                tap[posTab] = tap[posTab] == '0' ? '1' : '0'
+                break;
+            case '[':
+                if (tap[posTab] == '0') {
+                    while (cod[i] != ']') {
+                        i--
+                    }
+                }
+                break;
+            case ']':
+                if (tap[posTab] != '0') {
+                    while (cod[i] != '[') {
+                        i++
+                    }
+                }
+                break;
+        }
+    }
+
+    return tap.join("")
 }
