91968f4550f88c1b161c7a308805b55a9019f1c4
Joshua Saucedo <joshuansu0897@gmail.com>
2019-03-20 15:06:04 -0700

    Simple Fun #2

---
+++ CodeWars/Simple Fun #2: Circle of Numbers/JavaScript/index.js
@@ -0,0 +1,13 @@
+const myFunc = require('./code')
+
+console.log(myFunc(10, 2), 7)
+
+console.log(myFunc(10, 7), 2)
+
+console.log(myFunc(4, 1), 3)
+
+console.log(myFunc(6, 3), 0)
+
+console.log(myFunc(100, 86), 36)
+
+console.log(myFunc(164, 106), 24)
